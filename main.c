9c9812b37a8a0d624970dcf1b104c8d58424eaf3
zakkor <edward.partenie@gmail.com>
2020-12-03 10:52:51 +0200

    make lexer actually work

--- main.c
+++ main.c
@@ -6,41 +6,46 @@
 
 #define LINE_SIZE 256
 
-int readfile(char*** readlines, char* path) {
-	FILE* fp = fopen(path, "r");
-	char** lines = malloc(1 * sizeof(char*));
-	for (int i = 0; ; i++) {
-		lines[i] = malloc(LINE_SIZE);
-		if (!fgets(lines[i], LINE_SIZE, fp)) {
-			fclose(fp);
-			*readlines = lines;
-			return i;
-		}
-		lines = realloc(lines, (i+2) * sizeof(char*));
-	}
-}
-
 struct token {
-	char* type;
-	char* data;
+	char* kind;
+	char* text;
 };
 
 void lex(char *path) {
 	FILE* fp = fopen(path, "r");
 	char buf[LINE_SIZE];
 	while (fgets(buf, LINE_SIZE, fp)) {
+		int findword = 0;
 		vec_char_t word;
 		vec_init(&word);
+		int findnumber = 0;
+		vec_char_t number;
+		vec_init(&number);
 
-		for (int i = 0; i < strlen(buf); i++) {
-			if (buf[i] == ' ') {
-				printf("IDENT %s\n", word.data);
+		for (int i = 0; i <= strlen(buf); i++) {
+			if (buf[i] == ' ' || buf[i] == '\n') {
+				if (findword) {
+					findword = 0;
+					printf("IDENT %s\n", word.data);
+				} 
+				if (findnumber) {
+					findnumber = 0;
+					printf("NUMBER %s\n", number.data);
+				}
 			} else if (buf[i] == '=') {
 				printf("ASSIGN\n");
 			} else if (buf[i] >= 'a' && buf[i] <= 'z') {
+				if (!findword) {
+					findword = 1;
+					vec_clear(&word);
+				}
 				vec_push(&word, buf[i]);
 			} else if (buf[i] >= '0' && buf[i] <= '9') {
-				printf("DIGIT\n");
+				if (!findnumber) {
+					findnumber = 1;
+					vec_clear(&number);
+				}
+				vec_push(&number, buf[i]);
 			}
 		}
 	}
